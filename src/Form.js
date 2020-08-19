1ff92c365a92317806e3ea4a1962b6e0a526d8fb
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-08-19 16:12:41 -0400

    added form skeleton

--- src/Form.js
+++ src/Form.js
@@ -3,6 +3,45 @@ import React from 'react';
 export default function Form() {
 
     return (
-        <div>Hey</div>
+        <form>
+            <div>
+                <label htmlFor="name">Name: </label>
+                <input
+                    value={values.name}
+                    onChange={onInputChange}
+                    name='name' 
+                    type="text" 
+                    id="name"
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email: </label>
+                <input
+                    value={values.email}
+                    onChange={onInputChange}
+                    name='email' 
+                    type="email" 
+                    id="email"
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password: </label>
+                <input 
+                    value={values.password}
+                    onChange={onInputChange}
+                    name='password'
+                    type="password" 
+                    id="password"
+                />
+            </div>
+            <div>
+                <label>Terms of Service: </label>
+                <input 
+                    type="check" 
+                    
+                />
+            </div>
+            <button>Submit</button>
+        </form>
     )
 }
